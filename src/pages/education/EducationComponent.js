0d6d54cd7fc639aeacf67ffac7b5c8ca1cb5f0bb
seancxy <seanchengxy13@gmail.com>
2022-12-14 11:02:00 +0800

    -Removed blog section -fixed upside down logo in education page

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -23,14 +23,15 @@ function Education(props) {
                 Education
               </h1>
               <h3 className="heading-sub-text" style={{ color: theme.text }}>
-                Basic Qualification and Certifications
+                Qualification and Certifications
               </h3>
               <p
                 className="experience-header-detail-text subTitle"
                 style={{ color: theme.secondaryText }}
               >
-                I actively participate in hackathons and other tech-related
-                activities. Below are some of my major certifications.
+                I am a Penultimate Student at National University of Singapore
+                pursuing a degree in Information Systems, Bachelor of Computing
+                with Honours.
               </p>
             </div>
           </div>
--- src/portfolio.js
+++ src/portfolio.js
@@ -549,13 +549,6 @@ const contactPageData = {
     description:
       "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
   },
-  blogSection: {
-    title: "Blogs",
-    subtitle:
-      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
-    link: "https://twitter.com/Harikrushn9",
-    avatar_image_path: "blogs_image.svg",
-  },
 };
 
 const projects = {
