6d545fcd90b7310c45d9f23f1539926732c98b13
seancxy <seanchengxy13@gmail.com>
2022-12-15 17:15:27 +0800

    - contact page

--- src/portfolio.js
+++ src/portfolio.js
@@ -468,7 +468,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "harikrushn.jpg",
+    profile_image_path: "xy.jpg",
     description:
       "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
   },
